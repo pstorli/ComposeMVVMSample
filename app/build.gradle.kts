d2ac5acac2ef0a2b062eba1f41fb8f838b151bf7
PETER STORLI <pstorli@gmail.com>
2025-12-11 20:07:01 -0800

    Initial Working Commit2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // To display Images from URL
+    implementation("io.coil-kt:coil-compose:2.7.0")
 }
--- app/src/main/java/com/pstorli/composemvvmsample/MainActivity.kt
+++ app/src/main/java/com/pstorli/composemvvmsample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,9 +12,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.pstorli.composemvvmsample.domain.model.ViewModel
 import com.pstorli.composemvvmsample.ui.theme.ComposeMVVMSampleTheme
 
 class MainActivity : ComponentActivity() {
+    val viewModel: ViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
